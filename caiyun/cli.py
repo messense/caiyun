4a12c2512cd5045519cffd1d8724b8cd8805550f
messense <messense@icloud.com>
2016-05-18 21:40:14 +0800

    Support filter by season

--- caiyun/cli.py
+++ caiyun/cli.py
@@ -17,7 +17,10 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',  # NOQA
     'Connection': 'keep-alive',
 }
-Resource = collections.namedtuple('Resource', ['name', 'url'])
+Resource = collections.namedtuple(
+    'Resource',
+    ['name', 'url', 'season', 'episode']
+)
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
@@ -39,7 +42,10 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
               help=u'百度云盘密码')
 @click.option('-f',
               '--format',
-              type=click.Choice(['HR-HDTV', 'MP4', 'HDTV', '720P', '1080P', 'WEB-DL', 'BD-720P', 'BD-1080P']),
+              type=click.Choice([
+                  'HR-HDTV', 'MP4', 'HDTV', '720P', '1080P', 'DVD',
+                  'WEB-DL', 'BD-720P', 'BD-1080P', 'DVDSCR', 'RMVB',
+              ]),
               default='HR-HDTV',
               show_default=True,
               help=u'资源格式')
@@ -54,8 +60,12 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
               default=u'/我的视频',
               show_default=True,
               help=u'百度云下载路径')
+@click.option('-s',
+              '--season',
+              default='',
+              help=u'第几季')
 def main(url, zimuzu_username, zimuzu_password, baidu_username,
-         baidu_password, format, type, path):
+         baidu_password, format, type, path, season):
     u"""字幕组资源下载至百度云盘
 
     URL: 字幕组资源下载页面地址
@@ -70,7 +80,8 @@ def main(url, zimuzu_username, zimuzu_password, baidu_username,
 
     success_count = 0
     failure_count = 0
-    resources = parse_resources(res.text, format, type)
+    seasons = season.split(',') if season else []
+    resources = parse_resources(res.text, format, type, seasons)
     pcs = baidupcsapi.PCS(baidu_username, baidu_password.encode('utf-8'))
     if not path.startswith(u'/'):
         path = u'/{}'.format(path)
@@ -78,7 +89,7 @@ def main(url, zimuzu_username, zimuzu_password, baidu_username,
         file_path = os.path.join(path, resource.name)
         try:
             pcs.add_download_task(resource.url, file_path)
-        except:
+        except (baidupcsapi.api.LoginFailed, baidupcsapi.api.CancelledError):
             failure_count += 1
             click.echo(u'添加下载任务失败：{}'.format(resource.name), err=True)
         else:
@@ -114,7 +125,7 @@ def zimuzu_login(username, password):
     click.echo(u'登录字幕组账号成功！')
 
 
-def parse_resources(source, format, type):
+def parse_resources(source, format, type, seasons):
     page = lxml.etree.HTML(source)
     media_list = page.xpath(
         '//div[@class="media-list"]/ul/li[@format="{}"]'.format(
@@ -125,7 +136,15 @@ def parse_resources(source, format, type):
         click.echo(u'找不过任何资源！')
         sys.exit(1)
 
+    seasons = set(seasons)
     for media in media_list:
+        # season == 0 means no season data
+        season = media.attrib.get('season', '0')
+        episode = media.attrib.get('episode', '0')
+        if seasons:
+            if season not in seasons:
+                continue
+
         name_a = media.xpath('div[@class="fl"]/a')
         if not name_a:
             continue
@@ -134,4 +153,9 @@ def parse_resources(source, format, type):
         if not link_a:
             continue
         url = link_a[0].attrib['href']
-        yield Resource(name=name, url=url)
+        yield Resource(
+            name=name,
+            url=url,
+            season=season,
+            episode=episode,
+        )
